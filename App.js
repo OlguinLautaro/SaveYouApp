206cbf9a7fbce5628eac6af63c9445bfcc6f5d8f
lautaro <lautaroolguin89@gmail.com>
2023-09-01 20:37:23 -0300

    Importando el crear usuario y el crear contraseña

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword} from 'firebase/auth';
 
 export default function App() {
   return (

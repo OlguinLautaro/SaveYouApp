72dbd89d077cf6b1c1b481bfe0162e2104c63952
lautaro <lautaroolguin89@gmail.com>
2023-09-26 17:54:17 -0300

    Creando una tabla igual a la de usuario que se llam todos

--- Base_De_Datos/servidor/schema.sql
+++ Base_De_Datos/servidor/schema.sql
@@ -8,3 +8,11 @@ CREATE TABLE users{
     email VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255)
 };
+
+CREATE TABLE todos{
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255),
+    completed BOOLEAN DEFAULT false,
+    user_id INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+};

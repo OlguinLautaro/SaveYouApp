6b863fab991520c89d895dbe3c94800ec8fc8b57
lautaro <lautaroolguin89@gmail.com>
2023-09-27 21:57:30 -0300

    Cambiando un icono

--- Base_De_Datos/servidor/schema.sql
+++ Base_De_Datos/servidor/schema.sql
@@ -2,17 +2,17 @@ CREATE DATABASE saveyouapp;
 
 USE saveyouapp;
 
-CREATE TABLE users{
+CREATE TABLE users(
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255),
     email VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255)
-};
+);
 
-CREATE TABLE todos{
+CREATE TABLE todos(
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(255),
     completed BOOLEAN DEFAULT false,
     user_id INT NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
-};
+);
